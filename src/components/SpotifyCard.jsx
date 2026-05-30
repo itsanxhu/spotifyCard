c78c13f5f501fd3c11f9cfbc07ff38a12628c326
anshu <anshhufx@gmail.com>
2026-05-30 21:43:39 +0530

    add shadow

--- src/components/SpotifyCard.jsx
+++ src/components/SpotifyCard.jsx
@@ -6,7 +6,7 @@ import AlbumCover from "./AlbumCover";
 
 const SpotifyCard = () => {
   return (
-    <div className="flex flex-col justify-between p-3.5 px-4 w-52 h-52 rounded-4xl bg-zinc-900 sm:p-4 sm:w-63 sm:h-104 sm:rounded-3xl">
+    <div className="flex flex-col justify-between p-3.5 px-4 w-52 h-52 rounded-4xl bg-zinc-900 cursor-pointer active:scale-95 transition-transform duration-200 shadow-2xl sm:p-4 sm:w-63 sm:h-104 sm:rounded-3xl">
       <AlbumCover />
       <TitleName />
       <div className="flex flex-col justify-center gap-2 sm:pb-3">
